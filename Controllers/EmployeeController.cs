e603e28fbdeb69e9ab4492d52c704bb462e2a7c1
yogiraj <yogiraj.indulkar123@gmail.com>
2023-05-22 12:20:51 +0530

    assignment done

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,78 @@
+﻿using Assignment_1.Data;
+using Assignment_1.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Assignment_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly AssignDBContext _dbContext;
+       private readonly IConfiguration _configuration;
+
+        public EmployeeController(AssignDBContext dbContext, IConfiguration configuration)
+        {
+            _dbContext = dbContext;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("Login")]
+
+        public async Task<IActionResult> Login([FromBody] Emplyoee emplyoee)
+        {
+            if(emplyoee == null)
+            {
+                return BadRequest();
+            }
+
+            var user = await _dbContext.Employee.FirstOrDefaultAsync(e=>e.Email== emplyoee.Email && e.Password==emplyoee.Password);
+           
+            if (user == null)
+            {
+
+                return NotFound();
+            }
+
+            var token = CreateToken(user);
+
+            return Ok(new { user = user, token=token});
+
+
+        }
+        private string CreateToken(Emplyoee emplyoee)
+        {
+            List<Claim> claims = new List<Claim>
+             {
+               new Claim(ClaimTypes.Name, emplyoee.Email),
+               new Claim(ClaimTypes.Role, emplyoee.Role)
+              };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Key"]));
+
+            var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+
+              _configuration["JWT:Issuer"],
+
+              _configuration["JWT:Audience"],
+
+              claims,
+
+              expires: DateTime.Now.AddDays(10),
+
+              signingCredentials: signIn);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+    }
+}
---
+++ Data/AssignDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Assignment_1.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment_1.Data
+{
+    public class AssignDBContext: DbContext
+    {
+        public AssignDBContext() { }
+        public AssignDBContext(DbContextOptions<AssignDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Emplyoee> Employee { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=LAKKI;Initial Catalog=assignment1;Integrated Security=True");
+            }
+        }
+    }
+}
